ed81b937c7bb39e5430695f2c70bd93dc2a62d3a
infoomics <sbx_94@hotmail.com>
2023-02-08 17:33:43 +0000

    initial completion

--- 0_katas/__tests__/counter-intelligence.test.js
+++ 0_katas/__tests__/counter-intelligence.test.js
@@ -1,7 +1,24 @@
 const { counterIntelligence } = require('../counter-intelligence');
 
 describe('counterIntelligence()', () => {
-  it('', () => {
-    // first test here
+  test('final letter', () => {
+    expect( counterIntelligence('Y') ).toBe('X');
+  });
+
+  test('final letter; 1 word', () => {
+    expect( counterIntelligence('BCD Y') ).toBe('ABC X');
+  });
+
+  test('final letter; >1 word', () => {
+    expect( counterIntelligence('NKRRU YCKKZNKGXZ D') ).toBe('HELLO SWEETHEART X');
+  });
+
+  test('final letter; >1 word; punctuation', () => {
+    expect( counterIntelligence('ANVNVKNA CX YRLT DY IDLLQRWR XW HXDA FJH QXVN OAXV FXAT, MJAURWP G') )
+    .toBe('REMEMBER TO PICK UP ZUCCHINI ON YOUR WAY HOME FROM WORK, DARLING X');
+  });
+
+  test('final letter; >1 word; punctuation; lower cased', () => {
+    expect( counterIntelligence('ngbk g toik jge :) d') ).toBe('HAVE A NICE DAY :) X');
   });
 });
--- 0_katas/counter-intelligence.js
+++ 0_katas/counter-intelligence.js
@@ -1,6 +1,33 @@
 // Please do not change the name of this function
 const counterIntelligence = (str) => {
-  // Your code here
+  function isLetter(char) {
+    return /\w/.test(char);
+  }
+  
+  const shiftCharCodeBy = 'X'.charCodeAt() - str.slice(-1).charCodeAt();
+  let shiftedStr = '';
+
+  for (let i = 0; i < str.length; i++) {
+    const currentChar = str[i];
+
+    if ( !isLetter(currentChar) ) {
+      shiftedStr += currentChar;
+    } else {
+      let shiftedCharCode = currentChar.charCodeAt() + shiftCharCodeBy;
+
+      while ( shiftedCharCode < 'A'.charCodeAt() ) {
+        shiftedCharCode += 26;
+      }
+  
+      while ( shiftedCharCode > 'Z'.charCodeAt() ) {
+        shiftedCharCode -= 26;
+      }
+
+      shiftedStr += String.fromCharCode(shiftedCharCode);
+    }
+  }
+
+  return shiftedStr;
 };
 
 module.exports = { counterIntelligence };
