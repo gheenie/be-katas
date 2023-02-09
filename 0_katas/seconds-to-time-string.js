d79773b2dc7dc8ecebba56d5016929c378785a59
infoomics <sbx_94@hotmail.com>
2023-02-09 19:17:43 +0000

    add pseudocode

--- 0_katas/seconds-to-time-string.js
+++ 0_katas/seconds-to-time-string.js
@@ -1,6 +1,14 @@
 // Please do not change the name of this function
 const secondsToTimeString = (seconds) => {
-  // Your code here
+  // get numbers
+
+  //get singular or plural units
+
+  //  if built string is not empty += ', INPUT'
+
+  // if elements not 0 >1 and last element += ' and INPUT'  
+
+  // consider object, 2 arrays
 };
 
 module.exports = { secondsToTimeString };
