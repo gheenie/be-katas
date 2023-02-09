c374a42f5c7ebc40baccd073c554ef28398b0853
infoomics <sbx_94@hotmail.com>
2023-02-09 01:11:33 +0000

    initial completed code

--- 0_katas/__tests__/find-closing-parenthesis.test.js
+++ 0_katas/__tests__/find-closing-parenthesis.test.js
@@ -1,7 +1,27 @@
 const { findClosingParenthesis } = require('../find-closing-parenthesis')
 
 describe('findClosingParenthesis()', () => {
-  it('', () => {
-    // first test here
-  })
-})
+  test('empty string', () => {
+    expect( findClosingParenthesis('') ).toBe(-1);
+  });
+
+  test('no 2nd arg', () => {
+    expect( findClosingParenthesis('hello') ).toBe(-1);
+  });
+
+  test('1st (', () => {
+    expect( findClosingParenthesis('(hello)', 1) ).toBe(6);
+  });
+
+  test('>1st (; unnested inside; unnested outside', () => {
+    expect( findClosingParenthesis('Hello, (world). (Something Else).', 2) ).toBe(31);
+  });
+
+  test('>1st (; unnested inside; unnested outside', () => {
+    expect( findClosingParenthesis('Hello, (world, (foo) bar (something) else), foo (bar) cat', 3) ).toBe(35);
+  });
+
+  test('1st (; breadth nested inside; unnested outside', () => {
+    expect( findClosingParenthesis('Hello, (world, (foo) bar (something) else), foo (bar) cat', 1) ).toBe(41);
+  });
+});
