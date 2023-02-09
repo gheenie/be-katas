6c6af2d5157912d0756c58505b77c44f0721b692
infoomics <sbx_94@hotmail.com>
2023-02-09 17:10:01 +0000

    unseparate isLetter()

--- 0_katas/counter-intelligence.js
+++ 0_katas/counter-intelligence.js
@@ -1,16 +1,12 @@
 // Please do not change the name of this function
 const counterIntelligence = (str) => {
-  function isLetter(char) {
-    return /\w/.test(char);
-  }
-  
   const shiftCharCodeBy = 'X'.charCodeAt() - str.slice(-1).charCodeAt();
   let shiftedStr = '';
 
   for (let i = 0; i < str.length; i++) {
     const currentChar = str[i];
 
-    if ( !isLetter(currentChar) ) {
+    if ( !/\w/.test(currentChar) ) {
       shiftedStr += currentChar;
     } else {
       let shiftedCharCode = currentChar.charCodeAt() + shiftCharCodeBy;
