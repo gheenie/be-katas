e9e3f96127a41f4cbb9dca05dc0132d421900635
infoomics <sbx_94@hotmail.com>
2023-02-09 18:04:37 +0000

    complete task 3 and extra credit

--- 0_katas/__tests__/sum-consecutive-duplicates.test.js
+++ 0_katas/__tests__/sum-consecutive-duplicates.test.js
@@ -1,7 +1,41 @@
-const { sumConsecutiveDuplicates } = require('../sum-consecutive-duplicates');
+const { sumConsecutiveDuplicates, reduceConsecutives } = require('../sum-consecutive-duplicates');
 
 describe('sumConsecutiveDuplicates()', () => {
-  it('', () => {
-    // first test here
+  test('no consecutives', () => {
+    expect( sumConsecutiveDuplicates([1,2,3]) ).toEqual([1,2,3]);
+  });
+
+  test('1 consecutive', () => {
+    expect( sumConsecutiveDuplicates([1, 1, 2, 3]) ).toEqual([2,2,3]);
+  });
+
+  test('>1 consecutives; no non-consecs', () => {
+    expect( sumConsecutiveDuplicates([1, 1, 2, 2, 3, 3]) ).toEqual([2,4,6]);
+  });
+
+  test('>1 consecutives; with consecs', () => {
+    expect( sumConsecutiveDuplicates([1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1]) ).toEqual([2,2,4,2,3]);
+  });
+
+  test('function is pure', () => {
+    const input = [1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1];
+    sumConsecutiveDuplicates(input);
+    expect(input).toEqual([1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1]);
+  });
+});
+
+describe('reduceConsecutives()', () => {
+  test('duplicates exist after 1 run', () => {
+    expect( reduceConsecutives([1, 1, 2, 3]) ).toEqual([4,3]);
+  });
+
+  test('duplicates exist after >1 run', () => {
+    expect( reduceConsecutives([1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1]) ).toEqual([8,2,3]);
+  });
+
+  test('function is pure', () => {
+    const input = [1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1];
+    reduceConsecutives(input);
+    expect(input).toEqual([1, 1, 2, 1, 1, 1, 1, 2, 1, 1, 1]);
   });
 });
--- 0_katas/sum-consecutive-duplicates.js
+++ 0_katas/sum-consecutive-duplicates.js
@@ -1,6 +1,34 @@
 // Please do not change the name of this function
 const sumConsecutiveDuplicates = (arr) => {
-  // Your code here
+  const nums = JSON.parse( JSON.stringify(arr) );
+
+  let previousNum = nums.pop();
+  let subtotal = previousNum;
+  const summedNums = [];
+  
+  for (let i = nums.length - 1; i > -1; i--) {
+    const currentNum = nums[i];
+    
+    if (currentNum === previousNum) {
+      subtotal += nums.pop();
+    } else {
+      summedNums.push(subtotal);
+      previousNum = nums.pop();
+      subtotal = previousNum;
+    }
+
+    if (i === 0) summedNums.push(subtotal);
+  }
+
+  return summedNums.reverse();
 };
 
-module.exports = { sumConsecutiveDuplicates };
+function reduceConsecutives(arr) {
+  let reducedNums = sumConsecutiveDuplicates(arr);
+
+  if ([... new Set(arr)].length !== arr.length) reducedNums = reduceConsecutives(reducedNums);
+  
+  return reducedNums;
+}
+
+module.exports = { sumConsecutiveDuplicates, reduceConsecutives };
