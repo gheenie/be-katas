20331004291cf6d6729b6b93240050866fb52302
infoomics <sbx_94@hotmail.com>
2023-02-09 18:58:25 +0000

    initial code and tests

--- 1_data_manipulation/__tests__/treat-doggos.test.js
+++ 1_data_manipulation/__tests__/treat-doggos.test.js
@@ -1,7 +1,22 @@
 const treatDoggos = require('../treat-doggos.js');
 
 describe('treatDoggos', () => {
-  it('', () => {
-    // first test here
+  test('input not mutated', () => {
+    const dogs = [{ name: 'Otis', age: 3, hasRabies: true }];
+    treatDoggos(dogs);
+    expect(dogs).toEqual([{ name: 'Otis', age: 3, hasRabies: true }]);
+  });
+
+  test('references of outermost array and inner elements', () => {
+    const dogs = [{ name: 'Otis', age: 3, hasRabies: true }];
+    const newDogs = treatDoggos(dogs);
+    expect(dogs === newDogs).toBe(false);
+    expect(dogs[0] === newDogs[0]).toBe(false);
+  });
+
+  test('correct output', () => {
+    const dogs = [{ name: 'Otis', age: 3, hasRabies: true }];
+    const newDogs = treatDoggos(dogs);
+    expect(newDogs).toEqual([{ name: 'Otis', age: 3, hasRabies: false }]);
   });
 });
--- 1_data_manipulation/treat-doggos.js
+++ 1_data_manipulation/treat-doggos.js
@@ -1,3 +1,9 @@
-const treatDoggos = (dogs) => {};
+const treatDoggos = (dogs) => {
+    const newDogs = JSON.parse( JSON.stringify(dogs) );
+    
+    newDogs[0].hasRabies = false;
+
+    return newDogs;
+};
 
 module.exports = treatDoggos;
